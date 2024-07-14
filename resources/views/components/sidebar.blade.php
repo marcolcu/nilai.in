bb121ad331c568bcbf17df239ac9deaf806328a8
marcolcu <marcomelandri808@gmail.com>
2024-07-14 09:31:12 +0700

    [UPDATE] Membuat tampilan materi

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -118,7 +118,7 @@
                 </a>
             </li>
             <li>
-                <a href="/schedule" role="button"
+                <a href="/materi" role="button"
                     class="flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900">
                     <div class="grid mr-4 place-items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
@@ -128,7 +128,7 @@
                                 clip-rule="evenodd"></path>
                         </svg>
                     </div>
-                    Schedule
+                    Materi
                 </a>
             </li>
             <li>
