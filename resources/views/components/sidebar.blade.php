123a6e6b2e168f641f9ce5dd0c1da0ce39c2903b
marcolcu <marcomelandri808@gmail.com>
2024-07-15 00:58:54 +0700

    [UPDATE] Membuat tampilan ujian

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -132,7 +132,7 @@
                 </a>
             </li>
             <li>
-                <a href="/grades" role="button"
+                <a href="/ujian" role="button"
                     class="flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900">
                     <div class="grid mr-4 place-items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
@@ -142,7 +142,7 @@
                             </path>
                         </svg>
                     </div>
-                    Grades
+                    Ujian
                 </a>
             </li>
             <li>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\frontend\KursusController;
 use App\Http\Controllers\frontend\LectureController;
 use App\Http\Controllers\frontend\MataPelajaranController;
 use App\Http\Controllers\frontend\MateriController;
+use App\Http\Controllers\frontend\UjianController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,6 +30,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/kelas', [KelasController::class, 'index']);
     Route::get('/matapelajaran', [MataPelajaranController::class, 'index']);
     Route::get('/materi', [MateriController::class, 'index']);
+    Route::get('/ujian', [UjianController::class, 'index']);
 
 
     Route::get('/about', function () {
