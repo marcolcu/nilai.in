4c432ca3ab5f69d62881c9a252f5233ae8cc0bee
marcolcu <marcomelandri808@gmail.com>
2024-07-16 21:44:21 +0700

    [UPDATE] Memperbaiki login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -54,7 +54,7 @@
                                         required="">
                                 </div>
                                 <button type="submit"
-                                    class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign
+                                    class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign
                                     in</button>
                                     @if ($errors->any())
                                     <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50" id="alert" role="alert">
@@ -164,7 +164,7 @@
                                         required="">
                                 </div>
                                 <button type="submit" id="submit"
-                                    class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create
+                                    class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create
                                     Account</button>
                                 <p class="text-sm font-light text-gray-500">
                                     Already have an account? <button id="register"
